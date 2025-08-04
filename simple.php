e3257aa4d02b967362359aa0afb839f3df4bf305
chaichai0804 <aa0930510572@gmail.com>
2025-08-04 19:22:17 +0800

    Fix critical Dockerfile exclusion issue and add simple test

---
+++ simple.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Test</title>
+</head>
+<body>
+    <h1>Simple PHP Test</h1>
+    <p>PHP Version: <?php echo phpversion(); ?></p>
+    <p>Server Time: <?php echo date('Y-m-d H:i:s'); ?></p>
+    <p>✅ PHP is working!</p>
+</body>
+</html> 
