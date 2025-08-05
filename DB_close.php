d3d6dd01e1ab8756f97f4b5cd7bac6fc4b72b028
chaichai0804 <aa0930510572@gmail.com>
2025-08-05 16:38:47 +0800

    資料庫TEST 5

--- DB_close.php
+++ DB_close.php
@@ -2,7 +2,10 @@
 // 檢查連接類型並正確關閉
 if ($link instanceof PDO) {
     $link = null; // PDO 連接會自動關閉
-} else {
+} elseif ($link instanceof mysqli) {
     mysqli_close($link);
+} else {
+    // 如果是 PDOWrapper 或其他類型，設為 null
+    $link = null;
 }
 ?>
